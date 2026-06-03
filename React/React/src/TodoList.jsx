6cbf681bac74f92c33969e01518c53fed72c5823
Ushant Singh <ushantsingh4@gmail.com>
2026-06-03 14:02:54 +0530

    Todos

--- React/React/src/TodoList.jsx
+++ React/React/src/TodoList.jsx
@@ -2,42 +2,33 @@ import React, { useEffect, useState } from 'react'
 
 const TodoList = () => {
     let [input, setInput] = useState("")
-    let [todos, setTodos] = useState(() => {
-        let data = localStorage.getItem("keys")
-        if (data) {
-            return JSON.parse(data)
-        }
-        return []
-    });
-
-    useEffect(() => {
-        localStorage.setItem("keys", JSON.stringify(todos))
-    }, [todos])
+    let [todos, setTodos] = useState([]);
 
     function del(id) {
-        let updatedData = todos.filter((a, b) => {
-            return id != b
-        })
-        setTodos(updatedData)
+        setTodos(todos.filter((a, b) => id !== b))
     }
 
-  return (
-    <div>
-      <h1>TodoList</h1>
-      <input onChange={(e)=>setInput(e.target.value)} type="text" placeholder='Enter a task'/>
-          <button onClick={() => setTodos([...todos, input])}>add</button>
-          {
-              todos.map((a, index) => {
-                  return (
-                      <div>
-                          <h3>{a}</h3>
-                          <button onClick={()=>del(index)}>Delete</button>
-                      </div>
-                  )
-              })
-          }
-    </div>
-  )
+    function edit(id) {
+        let editData = prompt("Edit your task:", todos[id])
+        if (editData !== null) {
+            setTodos(todos.map((a, b) => b === id ? editData : a))
+        }
+    }
+
+    return (
+        <div>
+            <h1>TodoList</h1>
+            <input onChange={(e) => setInput(e.target.value)} type="text" placeholder='Enter a task' />
+            <button onClick={() => setTodos([...todos, input])}>add</button>
+            {todos.map((a, index) => (
+                <div key={index}>
+                    <h3>{a}</h3>
+                    <button onClick={() => del(index)}>Delete</button>
+                    <button onClick={() => edit(index)}>Edit</button>
+                </div>
+            ))}
+        </div>
+    )
 }
 
 export default TodoList
